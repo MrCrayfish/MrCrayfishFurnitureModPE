25884b238f72f796e0afc6c4e35149d5b33c2ba6
minecraftmuse3 <minecraftmuse3@gmail.com>
2015-05-16 13:20:07 +1200

    Get rid of stupid thingys

--- obj/local/armeabi-v7a/objs/com.mrcrayfish.furniturepe/MCPE/client/renderer/texture/TextureUVCoordinateSet.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi-v7a/objs/com.mrcrayfish.furniturepe/MCPE/client/renderer/texture/TextureUVCoordinateSet.o: \
- jni/MCPE/client/renderer/texture/TextureUVCoordinateSet.cpp \
- jni/MCPE/client/renderer/texture/TextureUVCoordinateSet.h
-
-jni/MCPE/client/renderer/texture/TextureUVCoordinateSet.h:
--- obj/local/armeabi-v7a/objs/com.mrcrayfish.furniturepe/MCPE/world/item/ItemInstance.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi-v7a/objs/com.mrcrayfish.furniturepe/MCPE/world/item/ItemInstance.o: \
- jni/MCPE/world/item/ItemInstance.cpp jni/MCPE/world/item/ItemInstance.h
-
-jni/MCPE/world/item/ItemInstance.h:
