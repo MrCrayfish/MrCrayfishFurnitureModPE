ba68f839e27d71e11436a843152a841027f913ea
CrazyWolf23 <acooper340@gmail.com>
2015-05-19 23:35:23 -0400

    Update OvenRenderer.cpp

--- jni/Furniture/render/tile/renderers/OvenRenderer.cpp
+++ jni/Furniture/render/tile/renderers/OvenRenderer.cpp
@@ -6,9 +6,17 @@ void OvenRenderer::render(const TilePos& pos, FurnitureTile* tile, TileTessellat
 	int x = pos.x, y = pos.y, z = pos.z;
 	int data = tess->region->getData(x, y, z);
 
-	//Block
-	tess->setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+	//Main Block
+	tess->setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.65F, 0.85F);
 	tess->tessellateBlockInWorld(tile, {x, y, z});
+        
+        //Header
+        tess->setRenderBounds(0.0F, 0.65F, 0.0F, 1.0F, 0.9F, 0.1F);
+        tess->tessellateBlockInWorld(tile, {x, y, z});
+
+        //Door
+        tess->setRenderBounds(0.1F, 0.1F, 0.85F, 0.9F, 0.50F, 0.95F);
+        tess->tessellateBlockInWorld(tile, {x, y, z});
 
 	tess->useForcedUV = false;
 }
