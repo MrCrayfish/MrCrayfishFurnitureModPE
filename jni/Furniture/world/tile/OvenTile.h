b57aac9afb8001f70862bdeafde37fcbd589fcbf
CrazyWolf23 <acooper340@gmail.com>
2015-05-20 02:14:17 -0400

    Update OvenTile.h

--- jni/Furniture/world/tile/OvenTile.h
+++ jni/Furniture/world/tile/OvenTile.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "FurnitureTile.h"
+#include "RotatableTile.h"
 #include "../item/OvenItem.h"
 
-class OvenTile : public FurnitureTile {
+class OvenTile : public RotatableTile {
 public:
 	static int _id;
 	
@@ -11,8 +11,10 @@ public:
 
 	virtual const TextureUVCoordinateSet& getTexture(signed char, int);
 	virtual int getResource(int, Random*);
-        virtual bool use(Player*, int, int, int);
+	virtual void addAABBs(TileSource* region, int x, int y, int z, AABB const* posAABB, std::vector<AABB, std::allocator<AABB>>& pool);
+	virtual bool use(Player*, int, int, int);
 	
 private:
-        TextureUVCoordinateSet secondary_tex;
-};
+	TextureUVCoordinateSet secondary_tex;
+	TextureUVCoordinateSet terciary_tex;
+}
