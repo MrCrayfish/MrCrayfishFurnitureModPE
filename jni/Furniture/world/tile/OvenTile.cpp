aff03809934edd0663e7809c3ed1b739812ba06e
CrazyWolf23 <acooper340@gmail.com>
2015-05-20 13:45:31 -0400

    Update OvenTile.cpp

--- jni/Furniture/world/tile/OvenTile.cpp
+++ jni/Furniture/world/tile/OvenTile.cpp
@@ -3,17 +3,18 @@
 int OvenTile::_id = 33;
 
 OvenTile::OvenTile(int id, Material const* material) : RotatableTile(id, material) {
+	Tile::solid[id] = false;
 	Tile::lightBlock[id] = 0;
-	
-	setNameId("tileOven");
+
+	setNameId("ovenTile");
 	setSoundType(Tile::SOUND_STONE);
-	tex = getTextureUVCoordinateSet("quartz_block", 1);
-	secondary_tex = getTextureUVCoordinateSet("stone", 0);
-	terciary_tex = getTextureUVCoordinateSet("obsidian", 0);
+	setDestroyTime(1.0F);
+	tex = getTextureUVCoordinateSet("quartz_block", 0);
 }
 
-const TextureUVCoordinateSet& OvenTile::getTexture(signed char side, int data) {
-	return side == 0 ? tex : (side == 1 ? secondary_tex : terciary_tex);
+void OvenTile::addAABBs(TileSource* region, int x, int y, int z, AABB const* posAABB, std::vector<AABB, std::allocator<AABB>>& pool) {
+	addAABB(AABB(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F).move(x, y, z), posAABB, pool);
+	addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.65F, 0.5F, 0.0F, 1.0F, 1.1F, 1.0F).move(x, y, z), posAABB, pool);
 }
 
 bool OvenTile::use(Player* player, int x, int y, int z) {
@@ -21,11 +22,5 @@ bool OvenTile::use(Player* player, int x, int y, int z) {
 }
 
 int OvenTile::getResource(int data, Random* rand) {
-	return CabinetItem::_id;
-}
-
-void OvenTile::addAABBs(TileSource* region, int x, int y, int z, AABB const* posAABB, std::vector<AABB, std::allocator<AABB>>& pool) {
-	addAABB(AABB(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F).move(x, y, z), posAABB, pool);
-	addAABB(AABB(0.1F, 1.0F, 0.0F, 0.9F, 1.2F, 0.1F).move(x, y, z), posAABB, pool);
-        addAABB(AABB(0.2F, 0.1F, 1.0F, 0.8F, 0.9F, 1.1F).move(x, y, z), posAABB, pool);
+	return OvenItem::_id;
 }
