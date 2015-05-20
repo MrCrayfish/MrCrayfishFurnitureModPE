0f52abfbea6e6a5123b1c1763b2ab7bcb3d533f1
CrazyWolf23 <acooper340@gmail.com>
2015-05-19 23:09:43 -0400

    Update OvenTile.cpp

--- jni/Furniture/world/tile/OvenTile.cpp
+++ jni/Furniture/world/tile/OvenTile.cpp
@@ -4,15 +4,20 @@ int OvenTile::_id = 28;
 
 OvenTile::OvenTile(int id, Material const* material) : FurnitureTile(id, material) {
 	tex = getTextureUVCoordinateSet("iron_block", 0);
+        secondary_tex = getTextureUVCoordinateSet("stone", 0);
 	
 	setNameId("tileOven");
-	setSoundType(Tile::SOUND_WOOD);
+	setSoundType(Tile::SOUND_STONE);
 	Tile::solid[id] = false;
 	Tile::lightBlock[id] = 0;
 }
 
 const TextureUVCoordinateSet& OvenTile::getTexture(signed char side, int data) {
-	return tex;
+	return side == 0 ? tex : secondary_tex;
+}
+
+bool OvenTile::use(Player* player, int x, int y, int z) {
+	return false;
 }
 
 int OvenTile::getResource(int data, Random* rand) {
