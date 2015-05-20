ce0ac93a973e08c58211072cd1c5a380cd4160a6
CrazyWolf23 <acooper340@gmail.com>
2015-05-20 17:35:26 -0400

    Update OvenRenderer.cpp

--- jni/Furniture/render/tile/renderers/OvenRenderer.cpp
+++ jni/Furniture/render/tile/renderers/OvenRenderer.cpp
@@ -1,34 +1,26 @@
 #include "OvenRenderer.h"
 
 void OvenRenderer::render(const TilePos& pos, FurnitureTile* tile, TileTessellator* tess) {
-	tess->useForcedUV = true;
-	tess->forcedUV = tile->getTexture(0, 0);
 	int x = pos.x, y = pos.y, z = pos.z;
 	int data = tess->region->getData(x, y, z);
+	
+	tess->useForcedUV = true;
 
-	//Main Block
-	tess->setRenderBounds(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F);
+	tess->tex1(tess->getLightColor(pos));
+
+	tess->forcedUV = tile->getTextureUVCoordinateSet("quartz_block", 1);
+	this->setRotatedBounds(tess, data, 0.1, 0.0, 0.0, 0.9, 1.0, 1.0);
 	tess->tessellateBlockInWorld(tile, {x, y, z});
-        
-        //Header
-        tess->setRenderBounds(0.1F, 1.0F, 0.0F, 0.9F, 1.2F, 0.1F);
-        tess->tessellateBlockInWorld(tile, {x, y, z});        
+        this->setRotatedBounds(tess, data, 0.1, 1.0, 0.0, 0.9, 1.2, 0.1);
+	tess->tessellateBlockInWorld(tile, {x, y, z});
+	
+	tess->forcedUV = tile->getTextureUVCoordinateSet("stone", 0);
+	this->setRotatedBounds(tess, data, 0.2, 0.1, 1.0, 0.8, 0.9, 1.1);
+	tess->tessellateBlockInWorld(tile, {x, y, z});
 
-        //Door
-        tess->forcedUV = tile->getTexture(1, 0);
-        tess->setRenderBounds(0.2F, 0.1F, 1.0F, 0.8F, 0.9F, 1.1F);
-        tess->tessellateBlockInWorld(tile, {x, y, z});
-
-        //Stoves
-//        tess->forcedUV = tile->getTexture(2, 0);
-//        tess->setRenderBounds(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F);
-//        tess->tessellateBlockInWorld(tile, {x, y, z});
-//        tess->setRenderBounds(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F);
-//        tess->tessellateBlockInWorld(tile, {x, y, z});
-//        tess->setRenderBounds(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F);
-//        tess->tessellateBlockInWorld(tile, {x, y, z});
-//        tess->setRenderBounds(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F);
-//        tess->tessellateBlockInWorld(tile, {x, y, z});
+//	tess->forcedUV = tile->getTextureUVCoordinateSet("obsidian", 0);
+//	this->setRotatedBounds(tess, data, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+//	tess->tessellateBlockInWorld(tile, {x, y, z});
 
 	tess->useForcedUV = false;
 }
