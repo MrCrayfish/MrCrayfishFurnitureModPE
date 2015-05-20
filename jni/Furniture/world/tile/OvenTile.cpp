10c9252472ec600b328c185732148d197ef7d00e
CrazyWolf23 <acooper340@gmail.com>
2015-05-20 17:24:16 -0400

    Update OvenTile.cpp

--- jni/Furniture/world/tile/OvenTile.cpp
+++ jni/Furniture/world/tile/OvenTile.cpp
@@ -13,10 +13,10 @@ OvenTile::OvenTile(int id, Material const* material) : RotatableTile(id, materia
 }
 
 void OvenTile::addAABBs(TileSource* region, int x, int y, int z, AABB const* posAABB, std::vector<AABB, std::allocator<AABB>>& pool) {
-	addAABB(AABB(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F).move(x, y, z), posAABB, pool);
-	addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.65F, 0.5F, 0.0F, 1.0F, 1.1F, 1.0F).move(x, y, z), posAABB, pool);
+	addAABB(AABB(0.1F, 0.0F, 0.0F, 0.9F, 1.0F, 1.0F).move(x, y, z), posAABB, pool);
+	addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.1F, 1.0F, 0.0F, 0.9F, 1.2F, 0.1F).move(x, y, z), posAABB, pool);
+       addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.2F, 0.1F, 1.0F, 0.8F, 0.9F, 1.1F).move(x, y, z), posAABB, pool);
 }
-
 bool OvenTile::use(Player* player, int x, int y, int z) {
 	return false;
 }
