8a3a72317d69ca8944a88d92b52b31b11e4fcd63
CrazyWolf23 <acooper340@gmail.com>
2015-07-22 21:46:28 -0400

    Fixed Lots Of Oven/CookieJar Errors/Renders

--- jni/Furniture/world/tile/CookieJarTile.cpp
+++ jni/Furniture/world/tile/CookieJarTile.cpp
@@ -7,6 +7,7 @@ CookieJarTile::CookieJarTile(int id, Material const* material) : FurnitureTile(i
 	setSoundType(Tile::SOUND_GLASS);
 	Tile::solid[id] = false;
 	Tile::lightBlock[id] = 0;
+	renderPass = Tile::glass->renderPass;
 }
 
 int CookieJarTile::getResource(int data, Random* rand) {
